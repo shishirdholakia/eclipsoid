4bd9c15f177a7ba68fb562b3cb59ba5e865c2a3a
Shashank Dholakia <shashankdholakia1@gmail.com>
2024-05-03 01:11:37 +1000

    finer res gui

--- src/jax0planet/oblate_GUI.py
+++ src/jax0planet/oblate_GUI.py
@@ -17,7 +17,7 @@ oblate_lightcurve = jax.jit(legacy_oblate_lightcurve)
 params = {'period':300.456,
           'radius':0.1,
           'u':jnp.array([0.3,0.2]),
-          'f':0.0,
+          'f':0.1,
           'bo':0.8,
           'duration':0.4,
           'theta':0.0
@@ -46,7 +46,10 @@ t = jnp.linspace(-0.3, 0.3, 200)
 lc = jax.vmap(limb_dark_light_curve(orbit, params['u']))(t)
 oblate_lc = oblate_lightcurve(params, t)
 ax0.plot(t, oblate_lc, label='oblate_lightcurve')
-ax1.plot(t, oblate_lc-lc, label='oblate - circular')
+ax1.plot(t, (oblate_lc-lc-1)*1e6, label='oblate - circular')
+ax0.set_xlabel('Time')
+ax0.set_ylabel('Flux')
+ax1.set_ylabel('Residual (ppm)')
 # Update function
 def update(val):
     params['theta'] = s_theta.get()
@@ -61,16 +64,21 @@ def update(val):
     ax0.cla()
     ax1.cla()
     ax0.plot(t, oblate_lc, label='oblate_lightcurve')
-    ax1.plot(t, oblate_lc-lc, label='oblate - circular')
+    ax1.plot(t, (oblate_lc-lc-1)*1e6, label='oblate - circular')
     fig.canvas.draw_idle()
 
 # Start the main loop
 
 # Create sliders
-s_theta = tk.Scale(root, label='Theta', from_=0.1, to=jnp.pi, orient=tk.HORIZONTAL, command=update,resolution=0.1)
-s_impact = tk.Scale(root, label='Impact Param', from_=0.1, to=1.0, orient=tk.HORIZONTAL, command=update,resolution=0.1)
-s_radius = tk.Scale(root, label='Radius', from_=0.1, to=1.0, orient=tk.HORIZONTAL, command=update, resolution=0.1)
-s_f = tk.Scale(root, label='f', from_=0.1, to=1.0, orient=tk.HORIZONTAL, command=update, resolution=0.1)
+s_theta = tk.Scale(root, label='Theta', from_=0.0, to=jnp.pi, orient=tk.HORIZONTAL, command=update,resolution=0.1, length=500)
+s_impact = tk.Scale(root, label='Impact Param', from_=0.0, to=1.0, orient=tk.HORIZONTAL, command=update,resolution=0.01, length=500)
+s_radius = tk.Scale(root, label='Radius', from_=0.01, to=1.0, orient=tk.HORIZONTAL, command=update, resolution=0.01,  length=500)
+s_f = tk.Scale(root, label='f', from_=0.0, to=0.5, orient=tk.HORIZONTAL, command=update, resolution=0.01, length=500)
+
+s_theta.set(params['theta'])
+s_impact.set(params['bo'])
+s_radius.set(params['radius'])
+s_f.set(params['f'])
 
 s_theta.pack()
 s_impact.pack()
